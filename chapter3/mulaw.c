22f6a11d51063efa5b715c78c16a745b6c9a3d7a
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-18 00:31:21 -0400

    writing to stream in SDL to create simple synth

--- chapter3/mulaw.c
+++ chapter3/mulaw.c
@@ -15,6 +15,13 @@ typedef struct AudioData {
 } AudioData;
 SDL_AudioDeviceID device_id;
 
+typedef struct _MuLaw {
+    uint8_t sign;
+    uint8_t chord;
+    uint8_t step;
+    uint16_t quantization_level;
+} MuLaw;
+
 void callback(void *userdata, uint8_t *stream, int streamLength)
 {
     SDL_LockAudioDevice(device_id);
@@ -22,12 +29,20 @@ void callback(void *userdata, uint8_t *stream, int streamLength)
     if (audio->length == 0) return;
     uint32_t length = (uint32_t)streamLength;
     length = (length > audio->length ? audio->length : length);
-
     memcpy(stream, audio->pos, length);
-
     audio->pos += length;
     audio->length -= length;
     SDL_UnlockAudioDevice(device_id);
+}
+
+void toMuLaw(uint8_t idx, MuLaw *mu)
+{
+    uint8_t num = idx - 128;
+    mu->sign = num >> 7;
+    mu->chord = (num >> 4) & 7;
+    mu->step = num & 15;
+    mu->quantization_level = ((2 * mu->step) + 33) *
+                                 pow(2, mu->chord) - 33;
 }
 
 int main(void)
@@ -37,17 +52,17 @@ int main(void)
     memset(&wavSpec, 0, sizeof(SDL_AudioSpec));
     uint8_t * wavStart;
     uint32_t wavLength;
-
-    SDL_AudioSpec *data = SDL_LoadWAV(FILE_PATH, &wavSpec, &wavStart, &wavLength);
+    SDL_AudioSpec *data = SDL_LoadWAV(FILE_PATH, &wavSpec, &wavStart,
+                                      &wavLength);
 
     if(!data) {
         ERROR();
         return 1;
     }
+
     AudioData audio;
     audio.pos = wavStart;
     audio.length = wavLength;
-
     wavSpec.callback = callback;
     wavSpec.userdata = &audio;
     SDL_AudioSpec desired;
