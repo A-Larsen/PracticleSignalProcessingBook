e3882bd19df42ce678b72f714fb4fff3fbf47111
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-19 12:40:36 -0400

    header file to convert raw data to wav files

--- chapter3/mulaw.c
+++ chapter3/mulaw.c
@@ -1,11 +1,10 @@
-#include <SDL_audio.h>
 #include <stdio.h>
-#include <SDL.h>
 #include <memory.h>
+#include <stdint.h>
+#include <SDL_audio.h>
+#include <SDL.h>
 
 #define SDL_main main
-#include <stdint.h>
-
 #define FILE_PATH ".\\question3.1.wav"
 #define ERROR() fprintf(stderr, "%s\n", SDL_GetError())
 
@@ -23,23 +22,6 @@ typedef struct _MuLaw {
     uint16_t quantization_level;
 } MuLaw;
 
-void callback(void *userdata, uint8_t *stream, int streamLength)
-{
-    SDL_LockAudioDevice(device_id);
-    AudioData *audio = (AudioData *)userdata;
-    if (!(audio->spec.format & AUDIO_U16)) {
-        return;
-    }
-    if (audio->length == 0) return;
-    uint32_t length = (uint32_t)streamLength;
-    length = (length > audio->length ? audio->length : length);
-    // divide stream by 8 to get values from -8191 -> +8191
-    memcpy(stream, audio->pos, length);
-    audio->pos += length;
-    audio->length -= length;
-    SDL_UnlockAudioDevice(device_id);
-}
-
 void getMuLaw(uint8_t idx, MuLaw *mu)
 {
     uint8_t num = idx - 128;
@@ -50,11 +32,39 @@ void getMuLaw(uint8_t idx, MuLaw *mu)
                                  pow(2, mu->chord) - 33;
 }
 
+void callback(void *userdata, uint8_t *stream, int streamLength)
+{
+    SDL_LockAudioDevice(device_id);
+    AudioData *audio = (AudioData *)userdata;
+
+    if (!(audio->spec.format & AUDIO_U16)) {
+        return;
+    }
+
+    if (audio->length == 0) return;
+    uint32_t length = (uint32_t)streamLength;
+    length = (length > audio->length ? audio->length : length);
+    /* MuLaw mu; */
+    /* memset(&mu, 0, sizeof(MuLaw)); */
+
+    /* for (int i = 0; i < length; ++i) { */
+    /*     int16_t s = ((float)stream[i] / (float)0xFFFF) * 8191.0f; */
+    /*     for (int i = -128; i < 127; ++i) { */
+    /*         getMuLaw(i, &mu); */
+    /*     } */
+    /* } */
+
+    memcpy(stream, audio->pos, length);
+    audio->pos += length;
+    audio->length -= length;
+    SDL_UnlockAudioDevice(device_id);
+}
+
+
 int main(void)
 {
     SDL_Init(SDL_INIT_AUDIO);
     AudioData audio;
-    /* SDL_AudioSpec wavSpec; */
     memset(&audio, 0, sizeof(SDL_AudioSpec));
     memset(&audio.spec, 0, sizeof(SDL_AudioSpec));
     uint8_t * wavStart;
@@ -80,7 +90,6 @@ int main(void)
 
     SDL_PauseAudioDevice(device_id, 0);
     while(audio.length > 0) SDL_Delay(100);
-
     SDL_CloseAudioDevice(device_id);
     SDL_FreeWAV(wavStart);
     SDL_Quit();
